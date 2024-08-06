8aa4a1161ef9c24b75e5c5c898d92476915b8a4e
Vladimir K <chag120521@gmail.com>
2024-08-06 10:44:50 +0300

    Start

---
+++ js/mod1-1.js
@@ -0,0 +1,12 @@
+// let a = 10;
+// console.log(a);
+// a = a + 5;
+// console.log(a);
+// ==============
+
+// Проверка типа переменной
+let a = '234.32';
+
+const type = typeof a;
+console.log(type);
+
